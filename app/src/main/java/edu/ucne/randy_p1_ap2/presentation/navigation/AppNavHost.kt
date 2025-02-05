03e4805f1234daeb8f13716d38c7f44985575ec1
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-05 18:18:32 -0400

    Inicio de examen

--- app/src/main/java/edu/ucne/randy_p1_ap2/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/edu/ucne/randy_p1_ap2/presentation/navigation/AppNavHost.kt
@@ -3,17 +3,15 @@
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
+//import androidx.navigation.NavHostController
+
 
 
 
  @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AppNavHost(
-    navController: NavHostController,
+ //   navController: NavHostController,
 ) {
  /*   NavHost(
         navController = navController,
