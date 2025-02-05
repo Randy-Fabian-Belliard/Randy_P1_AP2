9a1918a1ded0bdf7b3e730f8b87b2af1e4c094f4
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-05 18:17:42 -0400

    Inicio de examen

--- app/src/main/java/edu/ucne/randy_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/randy_p1_ap2/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.randy_p1_ap2.presentation.navigation.AppNavHost
 import edu.ucne.randy_p1_ap2.ui.theme.Randy_P1_AP2Theme
 
 
@@ -15,8 +17,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Randy_P1_AP2Theme {
-
+                val navHost = rememberNavController()
+                AppNavHost(
+                    navController = navHost
+                )
             }
         }
     }
+
+
 }
---
+++ app/src/main/java/edu/ucne/randy_p1_ap2/presentation/navigation/AppNavHost.kt
@@ -0,0 +1,31 @@
+ package edu.ucne.randy_p1_ap2.presentation.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+
+
+
+ @RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+) {
+ /*   NavHost(
+        navController = navController,
+
+    ) {
+
+
+
+
+    }*/
+}
+
+
+
+
