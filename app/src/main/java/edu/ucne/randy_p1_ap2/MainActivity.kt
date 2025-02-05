de77d4acc73dadf0ee9c0f6056a3a08b2622606c
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-05 14:47:42 -0400

    Agregando MainActivity

--- app/src/main/java/edu/ucne/randy_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/randy_p1_ap2/MainActivity.kt
@@ -4,24 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.randy_p1_ap2.ui.theme.Randy_P1_AP2Theme
-import kotlinx.serialization.Serializable
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
