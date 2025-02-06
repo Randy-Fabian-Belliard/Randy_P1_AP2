a89aacddbb8928df036cb8e9d02b31f736128138
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-05 20:44:07 -0400

    Modificando AppModule

--- app/src/main/java/edu/ucne/randy_p1_ap2/data/di/AppModule.kt
+++ app/src/main/java/edu/ucne/randy_p1_ap2/data/di/AppModule.kt
@@ -1,18 +1,32 @@
 package edu.ucne.randy_p1_ap2.data.di
 
 import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.randy_p1_ap2.data.local.database.AppDb
 import javax.inject.Singleton
 
 
 @InstallIn(SingletonComponent::class)
 @Module
-object AppModule {
+object AppModule{
     @Provides
     @Singleton
-    fun provideContex( @ApplicationContext appContext : Context) = appContext
+    fun provideDb(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            AppDb::class.java,
+            "DishWasher.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+    @Provides
+    @Singleton
+    fun provideSistemasDao(AppDb: AppDb) = AppDb.sistemasDao()
+
+
+
 }
